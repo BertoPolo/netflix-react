bed0a8c0a1963b8e245e581fc8812dba20b8222d
BertoPolo <bertopolo91@gmail.com>
2022-03-23 15:03:04 +0100

    pass App to functional component

--- src/App.js
+++ src/App.js
@@ -1,156 +1,146 @@
-import { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import "./styles/styles.css";
-import { Container, Alert, Dropdown } from "react-bootstrap";
-import MyNavbar from "./components/MyNavbar";
-import MyFooter from "./components/MyFooter";
-import MovieList from "./components/MovieList";
+import { useState, useEffect } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import "./styles/styles.css"
+import { Container, Alert, Dropdown } from "react-bootstrap"
+import MyNavbar from "./components/MyNavbar"
+import MyFooter from "./components/MyFooter"
+import MovieList from "./components/MovieList"
 
-class App extends Component {
-  state = {
+const App = () => {
+  /*   state = {
     gallery1: [],
     gallery2: [],
     gallery3: [],
     searchResults: [],
     loading: true,
     error: false,
-  };
+  }; */
 
-  OMDB_URL = "http://www.omdbapi.com/?apikey=24ad60e9";
+  const [gallery1, setGallery1] = useState([])
+  const [gallery2, setGallery2] = useState([])
+  const [gallery3, setGallery3] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
 
-  componentDidMount = () => {
-    this.fetchMovies();
-  };
+  const OMDB_URL = "http://www.omdbapi.com/?apikey=24ad60e9"
 
-  fetchMovies = () => {
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
+  const fetchMovies = async () => {
     Promise.all([
-      fetch(this.OMDB_URL + "&s=harry%20potter")
+      fetch(OMDB_URL + "&s=harry%20potter")
         .then((response) => response.json())
         .then((responseObject) => {
           if (responseObject.Response === "True") {
-            this.setState({ gallery1: responseObject.Search });
+            setGallery1(responseObject.Search)
           } else {
-            this.setState({ error: true });
+            setIsError(true)
           }
         }),
-      fetch(this.OMDB_URL + "&s=avengers")
+      fetch(OMDB_URL + "&s=avengers")
         .then((response) => response.json())
         .then((responseObject) => {
           if (responseObject.Response === "True") {
-            this.setState({ gallery2: responseObject.Search });
+            setGallery2(responseObject.Search)
           } else {
-            this.setState({ error: true });
+            setIsError(true)
           }
         }),
-      fetch(this.OMDB_URL + "&s=star%20wars")
+      fetch(OMDB_URL + "&s=star%20wars")
         .then((response) => response.json())
         .then((responseObject) => {
           if (responseObject.Response === "True") {
-            this.setState({ gallery3: responseObject.Search });
+            setGallery3(responseObject.Search)
           } else {
-            this.setState({ error: true });
+            setIsError(true)
           }
         }),
     ])
-      .then(() => this.setState({ loading: false }))
+      .then(() => setIsLoading(false))
       .catch((err) => {
-        this.setState({ error: true });
-        console.log("An error has occurred:", err);
-      });
-  };
+        setIsError(true)
 
-  showSearchResult = async (searchString) => {
+        console.log("An error has occurred:", err)
+      })
+  }
+
+  const showSearchResult = async (searchString) => {
     if (searchString === "") {
-      this.setState({ error: false, searchResults: [] }, () => {
-        this.fetchMovies();
-      });
+      setIsError(false)
+      setSearchResults([])
+      fetchMovies()
     } else {
       try {
-        const response = await fetch(this.OMDB_URL + "&s=" + searchString);
+        const response = await fetch(OMDB_URL + "&s=" + searchString)
         if (response.ok) {
-          const data = await response.json();
+          const data = await response.json()
           if (data.Response === "True") {
-            this.setState({ searchResults: data.Search, error: false });
+            setSearchResults(data.Search)
+            setIsError(false)
           } else {
-            this.setState({ error: true });
+            setIsError(true)
           }
         } else {
-          this.setState({ error: true });
-          console.log("an error occurred");
+          setIsError(true)
+          console.log("an error occurred")
         }
       } catch (error) {
-        this.setState({ error: true });
-        console.log(error);
+        setIsError(true)
+        console.log(error)
       }
     }
-  };
+  }
 
-  render() {
-    return (
-      <div>
-        <MyNavbar showSearchResult={this.showSearchResult} />
-        <Container fluid className="px-4">
-          <div className="d-flex justify-content-between">
-            <div className="d-flex">
-              <h2 className="mb-4">TV Shows</h2>
-              <div className="ml-4 mt-1">
-                <Dropdown>
-                  <Dropdown.Toggle
-                    style={{ backgroundColor: "#221f1f" }}
-                    id="dropdownMenuButton"
-                    className="btn-secondary btn-sm dropdown-toggle rounded-0"
-                  >
-                    Genres
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu bg="dark">
-                    <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            </div>
-            <div>
-              <i className="fa fa-th-large icons"></i>
-              <i className="fa fa-th icons"></i>
+  return (
+    <div>
+      <MyNavbar showSearchResult={showSearchResult} />
+      <Container fluid className="px-4">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex">
+            <h2 className="mb-4">TV Shows</h2>
+            <div className="ml-4 mt-1">
+              <Dropdown>
+                <Dropdown.Toggle
+                  style={{ backgroundColor: "#221f1f" }}
+                  id="dropdownMenuButton"
+                  className="btn-secondary btn-sm dropdown-toggle rounded-0"
+                >
+                  Genres
+                </Dropdown.Toggle>
+                <Dropdown.Menu bg="dark">
+                  <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
           </div>
-          {this.state.error && (
-            <Alert variant="danger" className="text-center">
-              An error has occurred, please try again!
-            </Alert>
-          )}
-          {this.state.searchResults?.length > 0 && (
-            <MovieList
-              title="Search results"
-              movies={this.state.searchResults}
-            />
-          )}
-          {!this.state.error && !this.state.searchResults?.length > 0 && (
-            <>
-              <MovieList
-                title="Harry Potter"
-                loading={this.state.loading}
-                movies={this.state.gallery1.slice(0, 6)}
-              />
-              <MovieList
-                title="The Avengers"
-                loading={this.state.loading}
-                movies={this.state.gallery2.slice(0, 6)}
-              />
-              <MovieList
-                title="Star Wars"
-                loading={this.state.loading}
-                movies={this.state.gallery3.slice(0, 6)}
-              />
-            </>
-          )}
-          <MyFooter />
-        </Container>
-      </div>
-    );
-  }
+          <div>
+            <i className="fa fa-th-large icons"></i>
+            <i className="fa fa-th icons"></i>
+          </div>
+        </div>
+        {isError && (
+          <Alert variant="danger" className="text-center">
+            An error has occurred, please try again!
+          </Alert>
+        )}
+        {searchResults?.length > 0 && <MovieList title="Search results" movies={searchResults} />}
+        {!isError && !searchResults?.length > 0 && (
+          <>
+            <MovieList title="Harry Potter" loading={isLoading} movies={gallery1.slice(0, 6)} />
+            <MovieList title="The Avengers" loading={isLoading} movies={gallery2.slice(0, 6)} />
+            <MovieList title="Star Wars" loading={isLoading} movies={gallery3.slice(0, 6)} />
+          </>
+        )}
+        <MyFooter />
+      </Container>
+    </div>
+  )
 }
 
-export default App;
+export default App
